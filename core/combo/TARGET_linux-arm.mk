a0bd749a2562c6d84ca4f52cc9411888e7c417ed
VlikeBanana <AndrolinRoot@googlemail.com>
2013-11-02 23:04:08 -0700

    Add -Wformat

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -102,7 +102,8 @@ TARGET_GLOBAL_CFLAGS += \
 			-funwind-tables \
 			-fstack-protector \
 			-Wa,--noexecstack \
-			-Werror=format-security \
+                        -Wformat \
+                        -Werror=format-security \
 			-D_FORTIFY_SOURCE=2 \
 			-fno-short-enums \
 			$(arch_variant_cflags) \
